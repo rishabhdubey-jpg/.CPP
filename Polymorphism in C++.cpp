b3efee2e9a3d72a0e5ac8d70d7fc41b29a5a84b1
RISHABH DUBEY <rishabhdx06@gmail.com>
2025-11-27 10:01:54 +0530

    Add files via upload

---
+++ Polymorphism in C++.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*    
+    // Polymorphism:
+        - one name many forms
+        - eg. Function overloading, Operator overloading.
+        - eg. Virtual Functions.
+
+    // Polymorphism in C++ can be of two types:
+    1. Compile time polymorphism.
+       Compile time polymorphism in C++ is acheived using:
+       1.1 - Function Overloading.
+       1.2 - Operator Overloading.
+    2. Run time polymorphism.
+       Run time polymorphism in C++ is acheived using:
+       2.1 - Virtual Functions.
+
+*/       
+    return 0;
+}
